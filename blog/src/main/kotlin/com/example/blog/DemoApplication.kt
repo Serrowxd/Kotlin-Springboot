898b27eeed3c0d7d59108270ccb95856498de7f4
Kevin Jolley <serrowpls@gmail.com>
2019-08-12 13:24:10 -0500

    compiled project from vscode

--- blog/src/main/kotlin/com/example/blog/DemoApplication.kt
+++ blog/src/main/kotlin/com/example/blog/DemoApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BlogApplication
+class DemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<BlogApplication>(*args)
+	runApplication<DemoApplication>(*args)
 }
